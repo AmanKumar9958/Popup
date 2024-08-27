7bd6948c605de9583376b9fe4828762d87d4abfc
Aman Kumar <151604983+AmanKumar9958@users.noreply.github.com>
2024-08-27 06:44:24 +0000

    All done..

--- script.js
+++ script.js
@@ -1,26 +1,15 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Get the popup
-    var popup = document.getElementById("popupForm");       //FOR SELECTING THE POPUPFORM
+let popupBox = document.getElementById('popup');
+let closeIcon = document.getElementById('closeIcon')
 
-    // Show the popup
-    popup.style.display = "block";                          //SHOWING THE FORM
+document.addEventListener('DOMContentLoaded', function(){
+    popupBox.style.display = 'block';
+        
+    // close using Close Icon..
+    closeIcon.addEventListener('click', function(){
+        popupBox.style.display = 'none';
+    })
 
-    // Get the <span> element that closes the popup
-    var span = document.getElementsByClassName("close")[0]; //SELECTING THE CLOSE ICON
-
-    // When the user clicks on <span> (x), close the popup
-    span.onclick = function() {
-        popup.style.display = "none";                       //MAKING CLICK FUNCTION
+    window.onclick = function(){
+        popupBox.style.display = 'none';
     }
-
-    // When the user clicks anywhere outside of the popup, close it
-    window.onclick = function(event) {
-        if (event.target == popup) {
-            popup.style.display = "none";                   //POPUP CLOSE IF ANYONE CLICK OUTSIDE
-        }
-    }
-});
-
-
-
-
+})
